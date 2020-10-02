5330537063788812bd58eb5c7682e67adf2bff6a
Aidan T. Manning <periodicaidan@gmail.com>
2020-10-02 13:36:14 -0500

    changed the way the search query is constructed

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const searchParams = {
   searchDepth: "",
   searchBeginDate: "",
   searchEndDate: "",
-  searchQuery: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchParams.searchTerm}&api-key=cqAs97wG20GAPgLPCyAlYv1Ujuj8VfcU&begin_date=${searchParams.searchBeginDate}&end_date=${searchParms.searchEndDate}&page=${searchParams.searchDepth}`,
+  // searchQuery: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchParams.searchTerm}&api-key=cqAs97wG20GAPgLPCyAlYv1Ujuj8VfcU&begin_date=${searchParams.searchBeginDate}&end_date=${searchParms.searchEndDate}&page=${searchParams.searchDepth}`,
+
+  get searchQuery() {
+    let searchQuery = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${this.searchTerm}&api-key=cqAs97wG20GAPgLPCyAlYv1Ujuj8VfcU&page=${this.searchDepth}`;
+
+    this.searchBeginDate !== "" && (searchQuery += `&begin_date=${this.searchBeginDate}`);
+    this.searchEndDate !== "" && (searchQuery += `&end_date=${this.searchEndDate}`);
+
+    return searchQuery
+  }
 };
 
